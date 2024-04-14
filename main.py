53a418e84542699a77d7639d35a6286e17bbff72
Robert Ortiz <r.j.ortiz90@gmail.com>
2024-04-14 11:49:52 +0200

    Error handling integrated, snooze functionality

--- main.py
+++ main.py
@@ -3,16 +3,38 @@ import tkinter as tk
 from tkinter import messagebox, simpledialog
 import time
 
+def valid_input_time(alarm_date, alarm_hours):
+    valid_input = ['012345678914-:']
+    for _ in alarm_date:
+        if _ not in valid_input:
+            return False
+    for _ in alarm_hours:
+        if _ not in valid_input:
+            return False
+    return True
+
 def set_alarm():
-    alarm_date = simpledialog.askstring("Set Alarm", "Enter date (dd-mm-yyyy):")
-    alarm_time = simpledialog.askstring("Set Alarm", f"Enter time for {alarm_date} (hh:mm):")
-    message = simpledialog.askstring("Set Alarm", "Enter message:")
-    setting = f"{alarm_date} {alarm_time}"
-    return setting, message
+    while True:
+        alarm_date = simpledialog.askstring("Set Alarm", "Enter date (dd-mm-yyyy):")
+        alarm_time = simpledialog.askstring("Set Alarm", f"Enter time for {alarm_date} (hh:mm):")
+        try:
+            if valid_input_time(alarm_date, alarm_hours=alarm_time):
+                message = simpledialog.askstring("Set Alarm", "Enter message:")
+                setting = f"{alarm_date} {alarm_time}"
+                return setting, message
+            else:
+                tk.messagebox.showinfo(message="Please enter a valid date and time")
+        except TypeError:
+            tk.messagebox.showinfo(message="Please enter a valid date and time")
 
 
 def alarm(alarm_time, message):
-    alarm_time = dt.datetime.strptime(alarm_time, '%d-%m-%Y %H:%M')
+    try:
+        alarm_time = dt.datetime.strptime(alarm_time, '%d-%m-%Y %H:%M')
+    except (ValueError, TypeError):
+        tk.messagebox.showinfo(message='Please enter a valid date and time.')
+        alarm_time, message = set_alarm()
+        alarm_time = dt.datetime.strptime(alarm_time, '%d-%m-%Y %H:%M')
     while dt.datetime.now() < alarm_time:
         time.sleep(1)
     response = tk.messagebox.askyesno(title='Alarm', message=f"{message}, Would you like to cancel the alarm? "
@@ -23,8 +45,6 @@ def alarm(alarm_time, message):
         alarm_time += dt.timedelta(minutes=time_snooze)
         alarm(alarm_time.strftime('%d-%m-%Y %H:%M'), message)
 
+
 alarm_time, message = set_alarm()
 alarm(alarm_time, message)
-
-
-
